155f0bc83baa5adcfe99efa047dbce73f06d19f6
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 14:59:30 -0400

    completed loop with flow control + debugging puts

--- exercise4.rb
+++ exercise4.rb
@@ -1,2 +1,13 @@
 ## Exercise 4 -- W1D2
 
+100.times do |num|
+	if (num % 3.00) && (num % 5.00) == 0
+		puts "#{num}: BitMaker"
+	elsif num % 3.00 == 0
+		puts "#{num}: Bit"
+	elsif num % 5.00 == 0
+		puts "#{num}: Maker"
+	else
+		puts num
+	end
+end
