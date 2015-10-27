6c7cc653cbfbb07542cabe645fa985ab2fbb2b3a
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 15:03:05 -0400

    changed loop range to 1 --> 100, refined debugging output

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,12 @@
 ## Exercise 4 -- W1D2
 
-100.times do |num|
+(1..100).each do |num| ( num + 1 )
 	if (num % 3.00) && (num % 5.00) == 0
-		puts "#{num}: BitMaker"
+		puts "#{num} BitMaker"
 	elsif num % 3.00 == 0
-		puts "#{num}: Bit"
+		puts "#{num} => Bit"
 	elsif num % 5.00 == 0
-		puts "#{num}: Maker"
+		puts "#{num} => Maker"
 	else
 		puts num
 	end
