c3c4421d6b4d551cd4e9f6d6b78c5402e4058835
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 14:08:59 -0400

    completed ex1

--- exercise1.rb
+++ exercise1.rb
@@ -4,3 +4,39 @@ puts 2
 puts 2
 puts 2 != 3
 
+## String Literals
+
+puts 'Hello world'
+puts "Hellow world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+puts "Enter name"
+name = gets.chomp
+puts "Your name is #{name}"  # double quotes can use interpolation
+
+a = 20
+b = 65
+
+puts "The value of a is #{a}."
+puts "The value of b is #{b}."
+puts "The value of a plus b is #{a + b}."
+
+
+## Escape sequences
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+
+## Booleans
+
+# in irb
+
+
+## Symbols
+
+# in irb
