078e864ebffb29a67bb4c7b07257b3e619a89306
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 14:16:38 -0400

    ex2 initial commit

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+## Exercise 2 -- Variables and Assignmnet
+
+# ask user for cost of meal
+print "What is the price of your meal before tax?: "
+price = $stdin.gets.chomp.to_f
+puts "#{price.class} and #{price}"
+
+
+# add tip to total ammount
+good_tip = 0.20		# 20% is a good tip!
