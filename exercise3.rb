79f60337575cbd0ceeff0aa8fe154583bf825134
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 14:49:44 -0400

    added age intake and ternary output for displaying age

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,11 @@
 ## Exercise 3 -- W1D2
 
-puts "What is your name?"
+print "What is your name?\n> "
+name = $stdin.gets.chomp
+
+puts "Hi #{name}."
+print "How old are you?\n> "
+age = $stdin.gets.chomp.to_i
+
+print "#{name}, you are #{age} years old? "
+puts (age <= 20 ? "What a little baby!" : "What a geezer!")
