7b7c2d087c37af1eedc25f81022704f46b387871
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 15:22:44 -0400

    Changed priority of flow control ex: 10 == 'Maker' not 'BitMaker' as previous

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,12 @@
 ## Exercise 4 -- W1D2
 
 (1..100).each do |num|
-	if (num % 3.00) && (num % 5.00) == 0
-		puts "#{num} => BitMaker"
-	elsif num % 3.00 == 0
+	if num % 3.00 == 0
 		puts "#{num} => Bit"
 	elsif num % 5.00 == 0
 		puts "#{num} => Maker"
+	elsif (num % 3.00 == 0) && (num % 5.00 == 0)  # highest priority
+		puts "#{num} => BitMaker"
 	else
 		puts num
 	end
