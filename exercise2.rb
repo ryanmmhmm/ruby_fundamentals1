f9cea28078e870f73aab0b9637a66ed4bd7b9454
ryanmmhmm <magowan.ryan@gmail.com>
2015-10-27 14:22:12 -0400

    questions 2 & 3 completed

--- exercise2.rb
+++ exercise2.rb
@@ -1,5 +1,6 @@
 ## Exercise 2 -- Variables and Assignmnet
 
+## question 1
 # ask user for cost of meal
 print "What is the price of your meal before tax?: "
 price = $stdin.gets.chomp.to_f
@@ -8,3 +9,15 @@ puts "#{price.class} and #{price}"
 
 # add tip to total ammount
 good_tip = 0.20		# 20% is a good tip!
+puts "A good tip for this meal is #{price * (good_tip)}"
+
+
+## question 2
+a = "String"	# define a string
+b = 1					# define an integer
+
+puts "#{a + b.to_s}"  # convert integer to string to concatenate results
+
+
+## question 3
+puts "45628 * 7839 = #{45628 * 7839}"
